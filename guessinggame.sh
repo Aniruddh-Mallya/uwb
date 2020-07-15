b9bb954e94c6e65af9516b052dfeded69286f98b
Aniruddh-Mallya <46568559+Aniruddh-Mallya@users.noreply.github.com>
2020-07-15 21:35:09 +0530

    Add files via upload

---
+++ guessinggame.sh
@@ -0,0 +1,21 @@
+echo "Hello User!"
+echo "Can you guess the number of files contained in the directory?" 
+response=0
+echo "Enter a value:"
+while [[ $response -ne 3 ]]
+do
+read response 
+echo "You entered: $response"
+if [[ $response -eq 3 ]]
+then echo "Congratulations!"
+elif [[ $response -lt 3 ]]
+then echo "Too low"
+echo "Pls guess once again:"
+else
+echo "Too high"
+echo "Pls guess once again:"
+fi 
+done
+echo "Exiting..."
+
+
